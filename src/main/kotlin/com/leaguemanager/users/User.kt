25757556f8e7768a6c9445d358d40ec9829471f6
aanciaes <m.anciaes@campus.fct.unl.pt>
2018-03-06 23:01:14 +0000

    User model first version

---
+++ src/main/kotlin/com/leaguemanager/users/User.kt
@@ -0,0 +1,14 @@
+package com.leaguemanager.users
+
+import com.leaguemanager.league.model.League
+import javax.persistence.*
+import javax.validation.constraints.Email
+
+@Entity
+class User (@Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long,
+            var name: String, @Email var mail : String) {
+
+    @OneToMany(cascade = arrayOf(CascadeType.ALL), fetch = FetchType.LAZY)
+    @JoinColumn(name = "managerId")
+    var leagues: List<League> = mutableListOf()
+}
